188052582852f139409f67110f0beec8a3c28cdd
Eray Sönmez <sonmezeray@hotmail.com>
2024-10-27 22:39:35 +0300

    init commit

--- app/src/main/java/com/example/sehirplakakodu/MainActivity2.kt
+++ app/src/main/java/com/example/sehirplakakodu/MainActivity2.kt
@@ -11,7 +11,7 @@ class MainActivity2 : AppCompatActivity() {
     lateinit var binding:ActivityMain2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
         binding=ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
